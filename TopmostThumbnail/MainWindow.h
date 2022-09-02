5797f08fc249ecf9a0abe3b7b6c5a9685ca76a26
robmikh <rob.mikh@outlook.com>
2022-09-02 11:16:17 -0700

    move RegisterWindowClass to private

--- TopmostThumbnail/MainWindow.h
+++ TopmostThumbnail/MainWindow.h
@@ -4,9 +4,11 @@
 struct MainWindow : robmikh::common::desktop::DesktopWindow<MainWindow>
 {
 	static const std::wstring ClassName;
-	static void RegisterWindowClass();
 	MainWindow(std::wstring const& titleString, HWND windowToThumbnail);
 	LRESULT MessageHandler(UINT const message, WPARAM const wparam, LPARAM const lparam);
+
+private:
+	static void RegisterWindowClass();
 
 private:
 	HWND m_windowToThumbnail = nullptr;
